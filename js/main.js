838ece8868db323366d8ced69833543447a29a28
Shaghayegh Malekpour <mk.shaqa@gmail.com>
2021-09-01 19:08:25 +0430

    add side bar

--- js/main.js
+++ js/main.js
@@ -3,21 +3,34 @@ function textToElement(text ,el){
     let textNode = document.createTextNode(text)
     el.appendChild(textNode)
 }
-function creatCard(headText ,paraText, url){
+function creatCard(headText ,paraText, liText,url){
     const cardContainer = document.createElement("DIV")
     cardContainer.classList ="card-container"
-    cardContainer.style.backgroundImage = url
+    const cardBackground = document.createElement("DIV")
+    cardBackground.classList ="card-background"
+    cardBackground.style.backgroundImage = url
+
     const head = document.createElement("H3")
     const paragraph = document.createElement("P")
     const hpContainer = document.createElement("DIV")
-    
 
+    const input= document.createElement("input")
+    const label= document.createElement("label")
+    const liContainer = document.createElement("DIV")
+ 
+    cardContainer.appendChild(cardBackground)
     hpContainer.appendChild(head)
     hpContainer.appendChild(paragraph)
     cardContainer.appendChild(hpContainer);
 
+    liContainer.appendChild(input)
+    input.setAttribute("type", "checkbox");
+    liContainer.appendChild(label)
+    cardContainer.appendChild(liContainer)
+
 textToElement(headText ,head)
 textToElement(paraText, paragraph)
+textToElement(liText,label)
 return cardContainer;
 }
 
@@ -28,8 +41,14 @@ async function main(){
      json.forEach((item)=>{
         const card = creatCard(item.title ,
             "Lorem ipsum dolor sit amet consecteturNecessitatibus soluta",
+            item.completed,
             "https://picsum.photos/300/400 ")
             document.getElementsByTagName('main')[0].appendChild(card)
+        })
+        json.forEach((x)=>{
+            let innerLabel = x.completed;
+            innerLabel.eve
+            innerLabel ? checked : !checked
         })
         }catch(err){
         console.log(err);
